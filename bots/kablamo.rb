c6e92d2feffc521615fc3452becdaa23649ae264
Brady Diffner <bradydiffner@Bradys-iMac.local>
2020-02-26 10:43:11 -0800

    Fixed formatting in the hunting and attacking code.

--- bots/kablamo.rb
+++ bots/kablamo.rb
@@ -11,15 +11,16 @@ class Kablamo
 
     bot_x = bot_state.position.x
     bot_y = bot_state.position.y
- 
+
     enemy_x = game_state.enemies[0].position.x
     enemy_y = game_state.enemies[0].position.y
- 
+
     # charge battery
     if bot_state.available_energy < 5
        return :charge_battery
     end
 
+    # Doge the lasers
     if bot_x - 1 == enemy_x || bot_x + 1 == enemy_x
       if bot_y < enemy_y
         if game_state.enemies[0].facing == :north
@@ -55,7 +56,7 @@ class Kablamo
             if game_state.enemies[0].facing == :east
               if bot_x > 3 && bot_x < game_state.arena_size.width - 4 && bot_y > 3 && game_state.arena_size.height - 4
                 return :lunge
-              else  
+              else
                 return :reverse
               end
             end
@@ -63,19 +64,25 @@ class Kablamo
         end
       end
     end
- 
-   # Track or shoot enemy on the X axis
+
+   # Track enemy on the X axis
     if bot_x < enemy_x && bot_state.facing != :east
       return :face_east
     elsif bot_x > enemy_x && bot_state.facing != :west
       return :face_west
-    elsif bot_y == enemy_y
+    end
+
+    # Attack if enemy is already at the same y as me
+    if bot_y == enemy_y
         return :fire_laser
-    elsif bot_x != enemy_x
+    end
+
+    # Advance if otherwise
+    if bot_x != enemy_x
       return :advance
     end
-    
-    # Attack enemy on the Y axis
+
+    # Trac and attack enemy on the y axis
     if bot_y < enemy_y && bot_state.facing != :south
       return :face_south
     elsif bot_y > enemy_y && bot_state.facing != :north
@@ -84,5 +91,5 @@ class Kablamo
       return :fire_laser
     end
 
-  end 
-end 
+  end
+end
