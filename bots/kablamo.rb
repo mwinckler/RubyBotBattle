1ccbdfe10b48936eb4ac6cf7090645712f3d9e8b
Brady Diffner <bradydiffner@Bradys-iMac.lan>
2020-03-09 20:54:40 -0700

    Makes bot ingnore avoidence protocol if enemy's energy is lower than five. Also cleaned up some of the messages in the tracking code.

--- bots/kablamo.rb
+++ bots/kablamo.rb
@@ -21,30 +21,10 @@ class Kablamo
     end
 
     # Doge the lasers
-    if bot_x - 1 == enemy_x || bot_x + 1 == enemy_x
-      if bot_y < enemy_y
-        if game_state.enemies[0].facing == :north
-          if bot_x > 3 && bot_x < game_state.arena_size.width - 4 && bot_y > 3 && game_state.arena_size.height - 4
-            return :lunge
-          else
-            return :reverse
-          end
-        end
-      else
-        if bot_y > enemy_y
-          if game_state.enemies[0].facing == :south
-            if bot_x > 3 && bot_x < game_state.arena_size.width - 4 && bot_y > 3 && game_state.arena_size.height - 4
-              return :lunge
-            else
-              return :reverse
-            end
-          end
-        end
-      end
-    else
-      if bot_y - 1 == enemy_y || bot_y + 1 == enemy_y
-        if bot_x < enemy_x
-          if game_state.enemies[0].facing == :west
+    if game_state.enemies[0].available_energy < 5
+      if bot_x - 1 == enemy_x || bot_x + 1 == enemy_x
+        if bot_y < enemy_y
+          if game_state.enemies[0].facing == :north
             if bot_x > 3 && bot_x < game_state.arena_size.width - 4 && bot_y > 3 && game_state.arena_size.height - 4
               return :lunge
             else
@@ -52,8 +32,8 @@ class Kablamo
             end
           end
         else
-          if bot_x > enemy_x
-            if game_state.enemies[0].facing == :east
+          if bot_y > enemy_y
+            if game_state.enemies[0].facing == :south
               if bot_x > 3 && bot_x < game_state.arena_size.width - 4 && bot_y > 3 && game_state.arena_size.height - 4
                 return :lunge
               else
@@ -62,10 +42,32 @@ class Kablamo
             end
           end
         end
+      else
+        if bot_y - 1 == enemy_y || bot_y + 1 == enemy_y
+          if bot_x < enemy_x
+            if game_state.enemies[0].facing == :west
+              if bot_x > 3 && bot_x < game_state.arena_size.width - 4 && bot_y > 3 && game_state.arena_size.height - 4
+                return :lunge
+              else
+                return :reverse
+              end
+            end
+          else
+            if bot_x > enemy_x
+              if game_state.enemies[0].facing == :east
+                if bot_x > 3 && bot_x < game_state.arena_size.width - 4 && bot_y > 3 && game_state.arena_size.height - 4
+                  return :lunge
+                else
+                  return :reverse
+                end
+              end
+            end
+          end
+        end
       end
     end
 
-   # Track enemy on the X axis
+   # Track enemy on the x axis
     if bot_x < enemy_x && bot_state.facing != :east
       return :face_east
     elsif bot_x > enemy_x && bot_state.facing != :west
@@ -82,7 +84,7 @@ class Kablamo
       return :advance
     end
 
-    # Trac and attack enemy on the y axis
+    # Track and attack enemy on the y axis
     if bot_y < enemy_y && bot_state.facing != :south
       return :face_south
     elsif bot_y > enemy_y && bot_state.facing != :north
