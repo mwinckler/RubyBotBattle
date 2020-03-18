3f044d52f5bdfed6489d5d948a43604dd5d41b8e
Matt Winckler <himself@mattwinckler.com>
2020-03-17 19:33:38 -0700

    Make AnimationManager.render public
    The render method was erroneously marked as private, but it needs to be called from game.rb. This change makes it public again.

--- lib/views/animations/animation_manager.rb
+++ lib/views/animations/animation_manager.rb
@@ -37,15 +37,15 @@ module Views
         return @animations.empty?()
       end
 
+      def render(frame, render_offset)
+        @animations.each { |animation| animation.render(frame, render_offset) }
+        remove_completed_animations()
+      end
+
       private
 
       def remove_completed_animations()
         @animations.reject! { |animation| animation.complete?() }
-      end
-
-      def render(frame, render_offset)
-        @animations.each { |animation| animation.render(frame, render_offset) }
-        remove_completed_animations()
       end
 
       def animations_complete?()
